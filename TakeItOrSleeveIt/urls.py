88444ab7d0761d3379a4ae882a6d2e85aaf68d2b
KzMilligan <19816994+keziah55@users.noreply.github.com>
2019-09-08 15:46:07 +0100

    updating views

--- TakeItOrSleeveIt/urls.py
+++ TakeItOrSleeveIt/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('vote/', views.vote, name='vote'),
+#    path('vote/', views.vote, name='vote'),
 ]
--- TakeItOrSleeveIt/views.py
+++ TakeItOrSleeveIt/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the Take It Or Sleeve It index.")
+#    response = "Hello, world. You're at the Take It Or Sleeve It index."
+    response = "Choose your favourite cover!"
+    return HttpResponse(response)
 
 def results(request):
     response = "The rankings"
     return HttpResponse(response)
 
-def vote(request):
-    response = "Choose your favourite cover!"
-    return HttpResponse(response)
+#def vote(request):
+#    response = "Choose your favourite cover!"
+#    return HttpResponse(response)
