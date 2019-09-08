b4166084bb2b31d4c90bad1823cec26b5117a56d
KzMilligan <19816994+keziah55@users.noreply.github.com>
2019-09-08 13:52:19 +0100

    secret settings

--- albumratingsite/settings.py
+++ albumratingsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .secret_settings import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tg+4(2-l43kzij54*8u(mn&fk#wxv@8!08gm)qd4^pl8$vsj1_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
